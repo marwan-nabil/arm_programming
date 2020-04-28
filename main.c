3b44f1bea5af51dff5b4c43df9b99802283ac514
marwan-nabil <marwan.nabil.mohammed@gmail.com>
2020-04-28 02:18:18 +0200

    finished lesson 20

--- main.c
+++ main.c
@@ -17,8 +17,27 @@ int main()
     // blink the red led
     while(1)
     {
+        // atomic operations that directly write to 
+        // memory withoul any loading first
         GPIOF_AHB->DATA_Bits[GREEN_LED] = GREEN_LED;
         GPIOF_AHB->DATA_Bits[GREEN_LED] = 0U;
+        
+        // non-atomic operations, load-modify-write
+        // could interfere with the SysTick_Handler() ISR
+        // which also uses GPIOF_AHB->DATA
+        // GPIOF_AHB->DATA |= GREEN_LED;
+        // GPIOF_AHB->DATA &= (~ GREEN_LED);
+        
+        // we can solve this by disabling interrupts while 
+        // accessing the shared data
+        // __disable_interrupt();
+        // GPIOF_AHB->DATA |= GREEN_LED;
+        // __enable_interrupt();
+        
+        // __disable_interrupt();
+        // GPIOF_AHB->DATA &= (~ GREEN_LED);
+        // __enable_interrupt();
+        // atomic operations are better in any case
     }
     
     // return 0;
